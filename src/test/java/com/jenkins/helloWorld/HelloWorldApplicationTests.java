af5ad3bc2241711c0ebdeed336b5cde770303a93
Hrithik Kumar Sonar <hrithikkumar@monocept.com>
2023-11-02 13:01:16 +0530

    testing added

--- src/test/java/com/jenkins/helloWorld/HelloWorldApplicationTests.java
+++ src/test/java/com/jenkins/helloWorld/HelloWorldApplicationTests.java
@@ -1,13 +1,27 @@
 package com.jenkins.helloWorld;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class HelloWorldApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	public void testApi() throws Exception {
+		System.out.println("Testing of API working...");
+		mockMvc.perform(MockMvcRequestBuilders.get("/greet").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk()).andReturn();
 	}
 
 }
